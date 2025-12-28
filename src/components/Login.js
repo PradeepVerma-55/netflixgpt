38355686378d26178b18629e257f83612785323b
Pradeep Verma <pradeepnetcore@outlook.com>
2025-12-28 16:50:25 +0530

    Designing the Sign In Screen

---
+++ src/components/Login.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/9ddb442a-aca7-4d85-9cd1-dbed62f18f26/web/IN-en-20251222-TRIFECTA-perspective_a882efaa-75c8-4143-9dc1-4f9932a791ac_large.jpg"
+          alt="Netflix Logo"
+        ></img>
+      </div>
+      <form
+        className="w-3/12 absolute p-12 bg-black
+        my-36 mx-auto right-0 left-0
+        text-white rounded-lg bg-opacity-85">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input type="text" placeholder="Email Address"
+         className="p-4 my-2 w-full bg-gray-700" />
+
+        <input type="text" placeholder="Password" 
+         className="p-4 my-2 w-full bg-gray-700" />
+
+        <button className="p-4 my-4 bg-red-700 
+        w-full rounded-lg">Sign In</button>
+      
+        <p>New to Netflix? 
+          <span className="text-blue-600
+            cursor-pointer"> Sign Up Now</span></p>
+      </form>
+
+    </div>
+  );
+};
+
+export default Login;
