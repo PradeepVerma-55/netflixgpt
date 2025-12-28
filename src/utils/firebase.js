87530ba4d5eee064ffc2214b4b4fa09db7ea7975
Pradeep Verma <pradeepnetcore@outlook.com>
2025-12-28 20:58:58 +0530

    Deploying App to Production

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC3sOGXD3bhirtLuLqkGRKV7FRe-2LRKho",
+  authDomain: "react-netflix-a070f.firebaseapp.com",
+  projectId: "react-netflix-a070f",
+  storageBucket: "react-netflix-a070f.firebasestorage.app",
+  messagingSenderId: "907997300006",
+  appId: "1:907997300006:web:980faeca1d2b1166076de7",
+  measurementId: "G-546MGGPKDR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
